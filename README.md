15d8c7b1d8890a0c5fd6882a3e2ba0277d5c453f
Johnnie Tirado <tiradoyamin@gmail.com>
2021-11-05 20:52:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Codealo Web App Template
+
+This is the base template that we can use to create new projects with Codealo.
+
+Give a boilerplate code to our members so that they can go in and start programming without need for something else.
+
+Instructions to run each of the diferent frameworks should be in a `README.md` file.
+
+
---
+++ vue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Codealo logo" src="./assets/logo.png">
+  <HelloWorld msg="[Project Name]"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #011627;
+  margin-top: 60px;
+}
+</style>
